64613edcda453a08eb86579ff56671ff896bf145
Mubangizi <mubangizia22@gmail.com>
2024-11-12 11:36:20 +0300

    setup project

---
+++ app/helpers/authenticate.py
@@ -0,0 +1,36 @@
+from functools import wraps
+from jose import jwt
+from flask import request
+import os
+
+
+def jwt_required(fn):
+    @wraps(fn)
+    def wrapper(*args, **kwargs):
+
+        access_token = request.headers.get('Authorization')
+
+        if access_token is None:
+            return dict(message='Access token was not supplied'), 401
+        try:
+            token = access_token.split(' ')[1]
+            if (access_token.split(' ')[0] != "Bearer"):
+                return dict(message="Bad Authorization header. Expected value 'Bearer <JWT>'"), 422
+
+            jwt.decode(token, os.getenv(
+                'FLASK_APP_SALT'), algorithms=['HS256'])
+
+        except Exception as e:
+            print(e)
+            return dict(message="Access token is not valid or key"), 401
+
+        return fn(*args, **kwargs)
+    return wrapper
+
+
+def admin_required(fn):
+    @wraps(fn)
+    @jwt_required
+    def wrapper(*args, **kwargs):
+        return fn(*args, **kwargs)
+    return wrapper
---
+++ config/config.py
@@ -0,0 +1,46 @@
+import os
+
+
+class Base:
+    """ base config """
+
+    # main
+    SECRET_KEY = os.getenv("FLASK_APP_SECRET")
+    FLASK_APP_SALT = os.getenv("FLASK_APP_SALT")
+    PRODUCT_BASE_URL = os.getenv('PRODUCT_BASE_URL')
+
+
+class Development(Base):
+    """ development config """
+
+    DEBUG = True
+    SQLALCHEMY_DATABASE_URI = os.getenv(
+        "DATABASE_URI", "postgresql:///cranecloud")
+
+
+class Testing(Base):
+    """ test environment config """
+
+    TESTING = True
+    DEBUG = True
+    # use a separate db
+    SQLALCHEMY_DATABASE_URI = os.getenv(
+        "TEST_DATABASE_URI") or "postgresql:///cranecloud_test_db"
+
+
+class Staging(Base):
+    """ Staging config """
+
+    DEBUG = False
+    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URI")
+
+
+class Production(Base):
+    """ production config """
+
+    DEBUG = False
+    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URI")
+
+
+app_config = {"development": Development, "testing": Testing,
+              "staging": Staging, "production": Production}
